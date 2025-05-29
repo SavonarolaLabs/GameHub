f72dd71908b52bed94e1f67ebd4635fc4bbff21c
c8e4 <c8e4@proton.me>
2025-05-29 22:33:47 +0200

    cloudflare svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,7 +6,13 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// Optional: Configure routes that should be prerendered at build time
+			routes: {
+				include: ['/*'],
+				exclude: []
+			}
+		})
 	}
 };
 
