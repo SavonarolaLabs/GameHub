ee763a1995d6e958651cd3c986a1565c379d878e
c8e4 <c8e4@proton.me>
2025-05-30 02:22:33 +0200

    update static build

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,11 +7,10 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			// Optional: Configure routes that should be prerendered at build time
-			routes: {
-				include: ['/*'],
-				exclude: []
-			}
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html', // Enable SPA mode for dynamic routes
+			precompress: false
 		})
 	}
 };
