e66376ecdca7778c0b18e6a19a6d011db7287162
Savonarola <x@x.com>
2025-08-08 09:04:48 +0300

    added Finance Structure

---
+++ src/lib/theatersFinance.ts
@@ -0,0 +1,22 @@
+// Auto-generated on 2025-08-08T12:34:00.000Z
+export const finance = [
+	{
+		id: 7703035553,
+
+		/** годовые показатели */
+		years: [
+			{
+				year: 2024,
+				offBudget: 542_000_000, // внебюджет, ₽
+				budget: 228_000_000, // бюджет, ₽
+				dependence: 0.3 // доля бюджета (30 %)
+			},
+			{
+				year: 2025,
+				offBudget: 542_000_000,
+				budget: 228_000_000,
+				dependence: 0.3
+			}
+		]
+	}
+];
