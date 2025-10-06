31084f50639d7a8a849474e3a6c26986ec1c683e
Savonarola <x@x.com>
2025-10-06 10:18:36 +0300

    add fixes

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,22 @@
+// svelte.config.js
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: vitePreprocess(),
+const dev = process.argv.includes('dev');
+const repo = 'GameHub';
 
+export default /** @type {import('@sveltejs/kit').Config} */ ({
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html', // Enable SPA mode for dynamic routes
+			pages: 'docs', // <-- кладём сборку в /docs
+			assets: 'docs',
+			fallback: '404.html', // для GitHub Pages SPA
 			precompress: false
-		})
+		}),
+		paths: {
+			base: dev ? '' : `/${repo}` // важно для project pages
+		},
+		prerender: { entries: ['*'] }
 	}
-};
-
-export default config;
+});
